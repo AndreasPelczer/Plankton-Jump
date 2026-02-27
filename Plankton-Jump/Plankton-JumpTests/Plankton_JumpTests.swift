c74603ae5d4f0974e7df52ef9b2dabc1d4c730c3
Andreas Pelczer <134264285+AndreasPelczer@users.noreply.github.com>
2026-02-27 16:01:22 +0100

    Add files via upload

---
+++ Plankton-Jump/Plankton-JumpTests/Plankton_JumpTests.swift
@@ -0,0 +1,17 @@
+//
+//  Plankton_JumpTests.swift
+//  Plankton-JumpTests
+//
+//  Created by Andreas Pelczer on 27.02.26.
+//
+
+import Testing
+@testable import Plankton_Jump
+
+struct Plankton_JumpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
